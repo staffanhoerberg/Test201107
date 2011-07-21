f4cf02934eb8b1df3c3d4575a05dae57ac32e02e
Staffan <staffan@mon7.se>
2011-07-21 20:48:12 +0200

    ny testfil calc.rb

---
+++ calc.rb
@@ -0,0 +1,6 @@
+konstant = 1
+puts "Hello there, and what\'s your favorite number?"
+number = gets.chomp
+number2 = konstant.to_i + number.to_i
+
+puts "Your favorite number is " + number.to_i + ". I think " + number2 + " is higher and better!"
